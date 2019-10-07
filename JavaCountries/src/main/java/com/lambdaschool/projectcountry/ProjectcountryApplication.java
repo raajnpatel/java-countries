516a8400cf4d1675a0c86cc5beb0fd15f7677273
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-07 12:49:39 -0700

    Initial Commit - Project initiated

---
+++ JavaCountries/src/main/java/com/lambdaschool/projectcountry/ProjectcountryApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.projectcountry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectcountryApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProjectcountryApplication.class, args);
+    }
+
+}
